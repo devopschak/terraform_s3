d029e8de31d8d07f1f7c4ecd22895d723740eb66
deepti <dee@gmail.com>
2021-03-08 22:47:17 -0500

    loading s3 bucket code

---
+++ provider.tf
@@ -0,0 +1,7 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAISF2U5552VKMYL5Q"
+  secret_key = "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"
+}
+
---
+++ s3bucket.tf
@@ -0,0 +1,25 @@
+# Create a bucket
+
+resource "aws_s3_bucket" "b1" {
+  bucket = "deepti1301"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+# Upload an object 
+
+resource "aws_s3_bucket_object" "object" {
+    for_each = fileset("myfiles/", "*")
+   bucket = aws_s3_bucket.b1.id
+  key    = each.value
+  source = "myfiles/${each.value}"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("myfiles/${each.value}")
+}
